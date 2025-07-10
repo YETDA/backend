a71d36a92e84785db22773ee59f0d5f8c0b5125e
KHE <99ant@yonsei.ac.kr>
2025-07-10 17:47:16 +0900

    [ Feat ] : 34 사용자정보 구현

--- backend/src/main/java/com/funding/backend/domain/user/controller/UserController.java
+++ backend/src/main/java/com/funding/backend/domain/user/controller/UserController.java
@@ -1,29 +1,109 @@
 package com.funding.backend.domain.user.controller;
 
+import com.funding.backend.domain.user.dto.request.UserAccountInfo;
+import com.funding.backend.domain.user.dto.request.UserProfileUpdateRequest;
 import com.funding.backend.domain.user.dto.response.UserInfoResponse;
+import com.funding.backend.domain.user.dto.response.UserProfileResponse;
+import com.funding.backend.domain.user.email.service.EmailService;
 import com.funding.backend.domain.user.entity.User;
 import com.funding.backend.domain.user.repository.UserRepository;
+import com.funding.backend.domain.user.service.UserService;
 import com.funding.backend.security.jwt.RefreshTokenService;
 import com.funding.backend.security.jwt.TokenService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
+@Tag(name = "회원 정보 API")
 public class UserController {
 
     private final TokenService tokenService;
-    private final UserRepository userRepository;
     private final RefreshTokenService refreshTokenService;
+    private final UserRepository userRepository;
+    private final UserService userService;
+    private final EmailService emailService;
+
+    @GetMapping("/mypage")
+    @Operation(summary = "마이페이지 사용자 정보 조회", description = "사용자의 정보를 조회합니다.")
+    public ResponseEntity<UserProfileResponse> getMyPage() {
+        Long userId = tokenService.getUserIdFromAccessToken();
+        return ResponseEntity.ok(userService.getMyProfile(userId));
+    }
+
+    @PutMapping(value = "/mypage", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(summary = "마이페이지 사용자 정보 수정", description = "사용자의 정보를 수정합니다.")
+    public ResponseEntity<Void> updateMyPage(@RequestPart("info") UserProfileUpdateRequest request,
+                                             @RequestPart(value = "image", required = false) MultipartFile imageFile) {
+        Long userId = tokenService.getUserIdFromAccessToken();
+        userService.updateUserProfile(userId, request, imageFile);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/check-email")
+    @Operation(summary = "이메일 중복 확인", description = "사용자의 이메일 정보를 수정시 중복 확인을 합니다.")
+    public ResponseEntity<Boolean> checkEmail(@RequestParam(name = "email") String email) {
+        boolean exists = userService.checkEmailDuplication(email);
+        return ResponseEntity.ok(exists);
+    }
+
+    @PostMapping("/send-verification")
+    @Operation(summary = "이메일 인증 코드 메일 전송", description = "사용자의 이메일 정보를 수정시 이메일 인증 확인을 위해 인증 코드 메일을 전송합니다.")
+    public ResponseEntity<Void> sendVerification(@RequestParam(name = "email") String email) {
+        emailService.sendVerificationCode(email);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/verify-code")
+    @Operation(summary = "이메일 인증 코드 검증", description = "사용자의 이메일 정보를 수정시 입력한 이메일 인증 코드를 검증합니다.")
+    public ResponseEntity<Boolean> verifyCode(@RequestParam(name = "email") String email,
+                                              @RequestParam(name = "code") String code) {
+        boolean result = emailService.verifyCode(email, code);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/mypage/account")
+    @Operation(summary = "은행 정보 및 계좌 조회", description = "현재 로그인한 사용자의 은행명과 계좌번호를 조회합니다.")
+    public ResponseEntity<UserAccountInfo> getAccountInfo() {
+        Long userId = tokenService.getUserIdFromAccessToken();
+        UserAccountInfo response = userService.getBankInfo(userId);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/mypage/account")
+    @Operation(summary = "은행 정보 및 계좌 등록/수정", description = "사용자의 은행명과 계좌번호를 등록하거나 수정합니다.")
+    public ResponseEntity<Void> updateAccountInfo(@RequestBody UserAccountInfo request) {
+        Long userId = tokenService.getUserIdFromAccessToken();
+        userService.updateBankInfo(userId, request);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/mypage/account")
+    @Operation(summary = "은행 정보 및 계좌 삭제", description = "사용자의 은행명과 계좌번호를 삭제합니다.")
+    public ResponseEntity<Void> deleteAccountInfo() {
+        Long userId = tokenService.getUserIdFromAccessToken();
+        userService.deleteBankInfo(userId);
+        return ResponseEntity.ok().build();
+    }
 
     @GetMapping("/me")
+    @Operation(summary = "사용자 정보 전체 조회", description = "사용자의 모든 정보를 조회합니다.")
     public ResponseEntity<UserInfoResponse> getMyInfo(HttpServletRequest request) {
         Long userId = tokenService.getUserIdFromAccessToken();
 
@@ -35,6 +115,7 @@ public class UserController {
     }
 
     @PostMapping("/logout")
+    @Operation(summary = "로그아웃", description = "쿠키/토큰 삭제")
     public ResponseEntity<Void> logout(HttpServletRequest request, HttpServletResponse response) {
         // 1. accessToken에서 userId 추출
         Long userId = tokenService.getUserIdFromAccessToken();
--- backend/src/main/java/com/funding/backend/domain/user/service/UserService.java
+++ backend/src/main/java/com/funding/backend/domain/user/service/UserService.java
@@ -1,19 +1,111 @@
 package com.funding.backend.domain.user.service;
 
+import com.funding.backend.domain.user.dto.request.UserAccountInfo;
+import com.funding.backend.domain.user.dto.request.UserProfileUpdateRequest;
 import com.funding.backend.domain.user.dto.response.UserInfoResponse;
+import com.funding.backend.domain.user.dto.response.UserProfileResponse;
+import com.funding.backend.domain.user.email.service.EmailService;
+import com.funding.backend.domain.user.entity.User;
 import com.funding.backend.domain.user.repository.UserRepository;
+import com.funding.backend.global.utils.s3.S3Uploader;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+    private final S3Uploader s3Uploader;
+    private final EmailService emailService;
+
+    public UserProfileResponse getMyProfile(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("유저를 찾을 수 없습니다."));
+
+        return UserProfileResponse.builder()
+                .name(user.getName())
+                .email(user.getEmail())
+                .introduce(user.getIntroduce())
+                .portfolioAddress(user.getPortfolioAddress())
+                .image(user.getImage())
+                .build();
+    }
+
+    public void updateUserProfile(Long userId, UserProfileUpdateRequest request, MultipartFile imageFile) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("유저를 찾을 수 없습니다."));
+
+        // 이메일 변경이 있는 경우
+        if (!user.getEmail().equals(request.getEmail())) {
+            if (userRepository.existsByEmail(request.getEmail())) {
+                throw new RuntimeException("이미 사용 중인 이메일입니다.");
+            }
+            if (!emailService.isVerified(request.getEmail())) {
+                throw new RuntimeException("이메일 인증이 필요합니다.");
+            }
+
+            user.setEmail(request.getEmail()); // 인증된 경우만 저장
+        }
+
+        // 이미지가 새로 업로드된 경우
+        if (imageFile != null && !imageFile.isEmpty()) {
+            // 이전 이미지 삭제
+            if (user.getImage() != null) {
+                s3Uploader.deleteFile(user.getImage());
+            }
+
+            try {
+                String imageUrl = s3Uploader.uploadFile(imageFile);
+                user.setImage(imageUrl);
+            } catch (IOException e) {
+                throw new RuntimeException("이미지 업로드 실패");
+            }
+        }
+
+        user.setName(request.getName());
+        user.setIntroduce(request.getIntroduce());
+        user.setPortfolioAddress(request.getPortfolioAddress());
+
+        userRepository.save(user);
+    }
+
+    public boolean checkEmailDuplication(String email) {
+        return userRepository.existsByEmail(email);
+    }
 
     public UserInfoResponse getUserInfo(Long userId) {
         return userRepository.findById(userId)
                 .map(UserInfoResponse::from)
                 .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    @Transactional(readOnly = true)
+    public UserAccountInfo getBankInfo(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("사용자 없음"));
+
+        return new UserAccountInfo(user.getBank(), user.getAccount());
+    }
+
+    @Transactional
+    public void updateBankInfo(Long userId, UserAccountInfo request) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("사용자 없음"));
+
+        user.setBank(request.getBank());
+        user.setAccount(request.getAccount());
+    }
+
+    @Transactional
+    public void deleteBankInfo(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("사용자 없음"));
+
+        user.setBank(null);
+        user.setAccount(null);
     }
 }
