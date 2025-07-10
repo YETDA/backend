cb59924ac9667eab019a4374acd96034cdf46506
KIMB0B <axzsd96301@gmail.com>
2025-07-10 23:08:25 +0900

    [ build ] 35 : .env로 프로젝트 환경변수 import 작업 추가

--- backend/src/main/java/com/funding/backend/BackendApplication.java
+++ backend/src/main/java/com/funding/backend/BackendApplication.java
@@ -1,5 +1,6 @@
 package com.funding.backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -12,6 +13,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class BackendApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
         SpringApplication.run(BackendApplication.class, args);
     }
 
