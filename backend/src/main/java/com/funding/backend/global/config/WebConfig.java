c4046e8262d1df43f39edc7833a68863c34bf4c6
jihyun <jihyun3066@naver.com>
2025-07-11 21:32:53 +0900

    [ feat ] 68 : 카카오, 깃허브 설정 파일 수정

--- backend/src/main/java/com/funding/backend/global/config/WebConfig.java
+++ backend/src/main/java/com/funding/backend/global/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${custom.site.frontUrl}")
+    @Value("${custom.dev.frontUrl}")
     private String frontUrl; // ex: http://localhost:3000 or https://www.yetda.site(?)
 
     @Override
