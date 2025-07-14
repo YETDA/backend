f737f079b0c3c1e5fe73d53cda4467f63d942382
jihyun <jihyun3066@naver.com>
2025-07-15 01:13:24 +0900

    [ feat ] : 배포 주소 추가

--- backend/src/main/java/com/funding/backend/global/config/YetdaSecurityConfig.java
+++ backend/src/main/java/com/funding/backend/global/config/YetdaSecurityConfig.java
@@ -75,7 +75,7 @@ public class YetdaSecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("http://localhost:3000", "https://yetda.kro.kr"
-                ,"https://www.yetda.booktri.site"));
+                ,"https://www.yetda.booktri.site","https://www.yetfront.booktri.site"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.addAllowedHeader("*");
         config.setAllowCredentials(true); // 💡 쿠키 포함 허용 필수
