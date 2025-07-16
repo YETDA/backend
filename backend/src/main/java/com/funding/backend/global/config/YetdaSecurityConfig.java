bc246e74b54a7a8b1ce43349934091f36a8fc2b8
KHE <99ant@yonsei.ac.kr>
2025-07-16 21:59:07 +0900

    [ fix ] 86 불필요한 부분 삭제

--- backend/src/main/java/com/funding/backend/global/config/YetdaSecurityConfig.java
+++ backend/src/main/java/com/funding/backend/global/config/YetdaSecurityConfig.java
@@ -116,9 +116,7 @@ public class YetdaSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000", "https://yetda.kro.kr",
-                "https://localhost:3000",
-                "https://www.yetda.booktri.site", "https://www.yetfront.booktri.site"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "https://yetda.kro.kr"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.addAllowedHeader("*");
         config.setAllowCredentials(true); // 💡 쿠키 포함 허용 필수
